34ecec7d3deb47f7a3aa7ad6daa0692399240a1c
ms <>
2021-09-01 09:59:58 -0700

    Formatted read

--- src/app/shared/bowlinggame.model.ts
+++ src/app/shared/bowlinggame.model.ts
@@ -1,7 +1,10 @@
+import { GamerPersona } from "./gamerpersona.model";
+
 export interface BowlingGame{
     id: string;
     laneID: number;
     groupName: string;
     gameDate: Date;
     created: Date;
+    gamerPersonas: GamerPersona[]
 }
---
+++ src/app/shared/gamerpersona.model.ts
@@ -0,0 +1,4 @@
+export interface GamerPersona{
+    name: number;
+    bowlingFrames: String[]
+}
