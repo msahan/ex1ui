4a99a75aec013eb090e90445b83491e41c560d1e
ms <>
2021-09-03 18:08:52 -0700

    added formGroup controls for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,59 +22,65 @@ export class AppComponent implements OnInit , OnDestroy {
   private unsubscribe = new Subject<void>();
 
   constructor(private bowlingGameService : BowlinggameService , private fb: FormBuilder ) {
-    this.gameForms = fb.group({ games :  this.bowlingGames } ) ;
+    // this.gameForms = fb.group({ games :  this.bowlingGames } ) ;
+    this.gameForms = fb.group({ } ) ;
   }
 
   // constructor(private bowlingGameService : BowlinggameService ){}
 
   ngOnInit() {
     this.setBowlingGames();
+  }
 
-
-
-
+  private setBowlingGames() : void{
+    this.bowlingGameService.getBowlingGames().subscribe( 
+      ( response : BowlingGame[] ) => {
+        this.bowlingGames = response;
+        this.gameForms = this.createGameForm( this.bowlingGames );
+      },
+      (error: HttpErrorResponse ) => {
+        this.handleError(error);
+      }
+    );
   }
   
-  private createGameForm(): FormGroup {
+  private createGameForm(pBowlingGames : BowlingGame[]): FormGroup {
 
     var fgrp : FormGroup = new FormGroup({ gameFormArray: new FormArray([]) });  
-    // if( this.bowlingGames != undefined ){ 
-    //     this.bowlingGames.forEach(
-    //         (bowlingGame) => { 
-    //             const bgame =       new FormGroup({ 
-    //                 id:             new FormControl(bowlingGame.id),
-    //                 laneID:         new FormControl(bowlingGame.laneID),
-    //                 groupName:      new FormControl(bowlingGame.groupName ,  Validators.required ),
-    //                 gameDate:       new FormControl(bowlingGame.gameDate), 
-    //                 created:        new FormControl(bowlingGame.created), 
-    //                 gamerPersonas:  new FormArray( [] )
-    //               });
-    //               (fgrp.controls.gameFormArray as FormArray).push(bgame);
-    //         }
-    //     ) ;
-    // }
 
-    if( this.bowlingGames != undefined ){ 
-      this.bowlingGames.forEach(
-          (bowlingGame) => { 
-              const bgame =       new FormGroup({ 
-                  id:             new FormControl(bowlingGame.id),
-                  laneID:         new FormControl(bowlingGame.laneID),
-                  groupName:      new FormControl(bowlingGame.groupName ,  Validators.required ),
-                  gameDate:       new FormControl(bowlingGame.gameDate), 
-                  created:        new FormControl(bowlingGame.created), 
-                  gamerPersonas:  new FormArray( [] )
-                });
-                (fgrp.controls.gameFormArray as FormArray).push(bgame);
+    if( pBowlingGames != undefined ){ 
+      pBowlingGames.forEach(
+        (bowlingGame, index) => { 
+          const bgame =       new FormGroup( { 
+              // key:            index,
+              id:             new FormControl(bowlingGame.id),
+              laneID:         new FormControl(bowlingGame.laneID),
+              groupName:      new FormControl(bowlingGame.groupName ,  Validators.required ),
+              gameDate:       new FormControl(bowlingGame.gameDate), 
+              created:        new FormControl(bowlingGame.created), 
+              gamerPersonas:  new FormArray( [] )
+            } );
+            if( bowlingGame.gamerPersonas != undefined  && bowlingGame.gamerPersonas != null ){
+              bowlingGame.gamerPersonas.forEach( (gamer, index2) => {
+                    const bGamer = new FormGroup({
+                      name:      new FormControl(gamer.name ,  Validators.required ),
+                      // bowlingFrames
+                      }
+                    );
+                    (bgame.controls.gamerPersonas as FormArray).push(bGamer);
+                }
+              );
+            }
+            (fgrp.controls.gameFormArray as FormArray).push(bgame);
           }
       ) ;
-  }    
+    }    
 
-    fgrp.valueChanges.pipe(
-      debounceTime(500), // wait for 500 ms after last change before initiating save
-      switchMap( formValue => this.bowlingGameService.save(formValue)), //use mergeMap instead of switchMap
-      takeUntil(this.unsubscribe)
-    ).subscribe(() => console.log('Saved'))   
+    // fgrp.valueChanges.pipe(
+    //   debounceTime(500), // wait for 500 ms after last change before initiating save
+    //   switchMap( formValue => this.bowlingGameService.save(formValue)), //use mergeMap instead of switchMap
+    //   takeUntil(this.unsubscribe)
+    // ).subscribe(() => console.log('Saved'))   
 
     return fgrp;
 
@@ -103,17 +109,7 @@ export class AppComponent implements OnInit , OnDestroy {
 
   }
 
-  private setBowlingGames() : void{
-    this.bowlingGameService.getBowlingGames().subscribe( 
-      ( response : BowlingGame[] ) => {
-        this.bowlingGames = response;
-        this.gameForms = this.createGameForm();
-      },
-      (error: HttpErrorResponse ) => {
-        this.handleError(error);
-      }
-    );
-  }
+
 
 
   
@@ -133,5 +129,9 @@ export class AppComponent implements OnInit , OnDestroy {
   ngOnDestroy() {
     this.unsubscribe.next();
   }
+
+  public trackByFn(index: any, item: any) {
+    return index;
+ }
 
 }
--- src/app/shared/bowlinggame.model.ts
+++ src/app/shared/bowlinggame.model.ts
@@ -1,6 +1,7 @@
 import { GamerPersona } from "./gamerpersona.model";
 
 export interface BowlingGame{
+    // key: number;
     id: string;
     laneID: number;
     groupName: string;
